e6f1f9341e14e8d474a8738b9672d28ddea845c8
nguye714 <nguye714@msu.edu>
2023-10-13 16:23:26 -0400

    Move JSON plugins

--- R/main.R
+++ R/main.R
@@ -7,14 +7,14 @@ plugins <- fromJSON("plugins.json")
 
 trim_plugin_column <- function(df, plugin_name="html-slider-response", columns=c()) {
   excludes <- plugins[[plugin_name]]	# Columns added by the plugin
-  
+
   df <- df %>% select(-c(excludes))	# Exclude above columns from data frame
   df <- df %>% select(-c(columns))	# Exclude any additional columns from data
-  
+
   # Get trial data
   df <- filter(df,
                df$trial_type == plugin_name)
-  
+
   return (df)
 }
 
@@ -22,9 +22,9 @@ filter_by_first_lang <- function(df, lang="en") {
   # Remove any leading or trailing white space around first_lang column
   # Convert value to title
   df$first_lang <- str_to_title(trimws(df$first_lang), locale="en")
-  
+
   df %>% distinct(first_lang) # show which distinct languages are there
   df <- filter(df, df$first_lang == "English")	#filter by English first language
-  
+
   return (df)
 }
